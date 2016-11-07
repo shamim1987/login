54a088ceb511dee514e50828335590f4e62ba356
shamim1987 <shamim.bhagat@gmail.com>
2016-11-07 12:43:44 +0800

    newblog

---
+++ .sql
@@ -0,0 +1,21 @@
+1. create table
+4 fields,id, name,age,address
+address != NOT NULL
+
+CREATE TABLE students (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name varchart(50) NOT NULL,
+  age INT NOT NULL,
+  address VARCHART(50)ills
+)
+insert data
+
+  insert into table_name VALUES(val1......valn)
+
+INSERT INTO students
+VALUES (
+  DEFAULT
+  'G C'
+  26,
+  '8 CL  Hill'
+)
